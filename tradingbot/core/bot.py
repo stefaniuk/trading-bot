cdde586bf4e4c4be881dab14268b5ea5a0625ab7
Federico123579 <federico123579@gmail.com>
2017-08-23 16:10:41 +0200

    added init Also updated bot to store object and not instance

--- tradingbot/core/bot.py
+++ tradingbot/core/bot.py
@@ -1,25 +1,25 @@
 from tradingAPI import API
 from getpass import getpass
 
-from color import *
-from config import Configurer
+from .color import *
+from .config import Configurer
 
 class Bot(object):
     def __init__(self):
-        self.api = API()
+        self.api = API
         self.config = Configurer("data.ini")
 
     def conf(self):
-        print(bold(blue("CONFIG")))
-        print("--------------------")
-        print("Add your credentials\n\
-               for Trading212")
+        print(bold(blue("------ CONFIG ------")))
+        print("Add your credentials")
+        print("for Trading212")
+        print(bold(blue("--------------------")))
         username = input(bold(yellow("Username: ")))
         password = getpass(bold(yellow("Password: ")))
         self.config.addLogin(username, password)
 
     def checkConf(self):
-        if self.config.checkFile():
+        if not self.config.checkFile():
             self.conf()
         else:
             self.config.read()
