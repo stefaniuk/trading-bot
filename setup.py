cf462ba5b21d7fb4d0b6dba56a399ebbbea6a3bb
Federico123579 <federico123579@gmail.com>
2017-08-23 16:09:32 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tradingbot",
+    version="0.1a0",
+    packages=['tradingbot'],
+    install_requires=[
+        'trading212api'
+    ],
+    zip_safe=False,
+    author="Federico Lolli",
+    author_email="federico123579@gmail.com",
+    description="Package to invest Trading212",
+    license="MIT",
+    keywords="trading bot",
+    url="https://github.com/federico123579/TradingBot",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: System :: Emulators'
+    ]
+)
