bbab9b649f3e3b5d970603244ead12bc6101859d
Federico123579 <federico123579@gmail.com>
2017-08-28 19:04:25 +0200

    Adjusted conf func

--- tradingbot/core/bot.py
+++ tradingbot/core/bot.py
@@ -3,34 +3,31 @@ from getpass import getpass
 
 from .color import *
 from .config import Configurer
+from .algorithm import *
 
 class Bot(object):
     def __init__(self):
+        self.api = API()
         self.config = Configurer("data.ini")
+        self.pivot = Pivot(self.api, self.config)
 
     def conf(self):
-        print(bold(blue("------ CONFIG ------")))
-        print("Add your credentials")
-        print("for Trading212")
-        print(bold(blue("--------------------")))
-        username = input(bold(yellow("Username: ")))
-        password = getpass(bold(yellow("Password: ")))
-        print(bold(blue("--------------------")))
-        print("Add your information")
-        print("for a monitoring")
-        print("account")
-        print(bold(blue("--------------------")))
-        username2 = input(bold(yellow("Username: ")))
-        password2 = getpass(bold(yellow("Password: ")))
-        stocks = input(bold(yellow("Favourite stocks (sep by spaces): "))).split(' ')
-        self.config.addLogin(username, password)
-        self.config.addMonitor(username2, password2, stocks)
-
-    def checkConf(self):
         if not self.config.checkFile():
-            self.conf()
+            print(info.header("config"))
+            print("Add your credentials")
+            print("for Trading212")
+            print(bold(blue("--------------------")))
+            username = input(info.user_input("Username: "))
+            password = getpass(info.user_input("Password: "))
+            print(bold(blue("--------------------")))
+            print("Add your information")
+            print("for a monitoring")
+            print("account")
+            print(bold(blue("--------------------")))
+            username2 = input(info.user_input("Username: "))
+            password2 = getpass(info.user_input("Password: "))
+            stocks = input(info.user_input("Favourite stocks (sep by spaces): ")).split(' ')
+            self.config.addLogin(username, password)
+            self.config.addMonitor(username2, password2, stocks)
         else:
             self.config.read()
-        
-    def start(self):
-        self.api = API()
