4e7b4cf4096de3b9e1c7e098e33d5e5cb6bc58f8
Federico Lolli <federico123579@gmail.com>
2017-12-07 11:24:28 +0100

    Removed unecessary files

--- scripts/chrome-install.sh
+++
@@ -1,8 +0,0 @@
-wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
-dpkg -i google-chrome-stable_current_amd64.deb
-apt-get install -y -f
-dpkg -i google-chrome-stable_current_amd64.deb
-wget "http://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
-mv chromedriver /usr/bin/chromedriver
-rm chromedriver_linux64.zip
